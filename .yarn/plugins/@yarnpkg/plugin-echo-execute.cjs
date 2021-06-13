30cab79a1a9ec0201dd224ddd0420bf75745127a
Marnes <leaumar@mailbox.org>
2021-06-13 15:13:56 +0200

    release 4

--- .yarn/plugins/@yarnpkg/plugin-echo-execute.cjs
+++ .yarn/plugins/@yarnpkg/plugin-echo-execute.cjs
@@ -2,7 +2,7 @@
 module.exports = {
 name: "@yarnpkg/plugin-echo-execute",
 factory: function (require) {
-var plugin;(()=>{"use strict";var e={d:(t,r)=>{for(var o in r)e.o(r,o)&&!e.o(t,o)&&Object.defineProperty(t,o,{enumerable:!0,get:r[o]})},o:(e,t)=>Object.prototype.hasOwnProperty.call(e,t),r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},t={};(()=>{e.r(t),e.d(t,{default:()=>r});const r={hooks:{wrapScriptExecution:async(e,t,r,o,n)=>()=>{const t=n.script.startsWith("yarn ")?n.script:"yarn "+n.script;return console.info(`${o}> ${t}`),e()}}}})(),plugin=t})();
+var plugin;(()=>{"use strict";var o={d:(t,e)=>{for(var r in e)o.o(e,r)&&!o.o(t,r)&&Object.defineProperty(t,r,{enumerable:!0,get:e[r]})},o:(o,t)=>Object.prototype.hasOwnProperty.call(o,t),r:o=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(o,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(o,"__esModule",{value:!0})}},t={};o.r(t),o.d(t,{default:()=>r});const e=require("@yarnpkg/core"),r={hooks:{wrapScriptExecution:async(o,t,r,a,n)=>async()=>(await e.StreamReport.start({configuration:t.configuration,json:!1,includeFooter:!1,stdout:n.stdout},async o=>{const r=e.formatUtils.applyColor(t.configuration,a,e.formatUtils.Type.NAME),i=e.formatUtils.applyColor(t.configuration,n.script,e.formatUtils.Type.CODE);o.reportInfo(e.MessageName.UNNAMED,`executing [${r}]: ${i}`)}),o())}};plugin=t})();
 return plugin;
 }
 };
