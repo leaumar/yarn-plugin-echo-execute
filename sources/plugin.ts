6a934c13a4b4bd823030fe9743d86801d7cba0c5
Angelo Tata <angelo@pave.com>
2021-06-13 15:07:15 +0200

    Replace console.log with StreamReport
    closes #1

--- sources/plugin.ts
+++ sources/plugin.ts
@@ -1,8 +1,26 @@
-import type {Plugin, Hooks} from "@yarnpkg/core";
+import {Plugin, Hooks, MessageName, StreamReport, formatUtils} from "@yarnpkg/core";
 
 const wrapScriptExecution: Hooks["wrapScriptExecution"] = async (executor, project, locator, scriptName, extra) => {
-    return () => {
-        console.info(`${scriptName}> ${extra.script}`);
+    return async () => {
+        await StreamReport.start(
+            {
+                configuration: project.configuration,
+                json: false,
+                includeFooter: false,
+                stdout: extra.stdout
+            },
+            async (report) => {
+                report.reportInfo(
+                    MessageName.UNNAMED,
+                    formatUtils.applyColor(
+                        project.configuration,
+                        `[${scriptName}] ${extra.script}`,
+                        formatUtils.Type.NO_HINT
+                    )
+                );
+            }
+        );
+
         return executor();
     };
 };
