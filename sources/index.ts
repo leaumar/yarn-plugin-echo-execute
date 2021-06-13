a54ed12e498dcb3cc1f9a59c5d94e03fea14e9be
Marnes <leaumar@mailbox.org>
2021-06-13 15:04:57 +0200

    isolate plugin code from export concern
    the export is a constant warning mark

--- sources/index.ts
+++ sources/index.ts
@@ -1,15 +1,3 @@
-import type {Plugin, Hooks} from "@yarnpkg/core";
-
-const plugin: Plugin<Hooks> = {
-    hooks: {
-        wrapScriptExecution: async (executor, project, locator, scriptName, extra) => {
-            return () => {
-                const command = extra.script.startsWith("yarn ") ? extra.script : `yarn ${extra.script}`;
-                console.info(`${scriptName}> ${command}`);
-                return executor();
-            };
-        }
-    }
-};
+import {plugin} from "./plugin";
 
 export default plugin;
---
+++ sources/plugin.ts
@@ -0,0 +1,13 @@
+import type {Plugin, Hooks} from "@yarnpkg/core";
+
+export const plugin: Plugin<Hooks> = {
+    hooks: {
+        wrapScriptExecution: async (executor, project, locator, scriptName, extra) => {
+            return () => {
+                const command = extra.script.startsWith("yarn ") ? extra.script : `yarn ${extra.script}`;
+                console.info(`${scriptName}> ${command}`);
+                return executor();
+            };
+        }
+    }
+};
